5b84bd191dd2c8302c3e4e77e337ed190d438eda
M4TY <matyhimmer@gmail.com>
2021-12-14 11:30:07 +0100

    biscuits

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
+function onLoad() {
+    var iframe = document.getElementById("frame");
+    var cookies = document.cookie;
+    var cookie = cookies.split(";");
+    var form = cookie[0].split("=");
+    console.log(form[1]);
+    iframe.src = form[1];
+}
+
 function loadFrame(subpage) {
     var iframe = document.getElementById("frame");
     iframe.src = subpage;
+    document.cookie = `currentFrame=${subpage}`;
 }
