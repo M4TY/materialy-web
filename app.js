fd8c0adff32c2df8486a69d7e4ea8944ad97fa6c
Maty <35110087+M4TY@users.noreply.github.com>
2021-12-15 08:36:34 +0100

    fix 404 error

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 function loaded() {
-    var iframe = document.getElementById("frame");
-    var cookies = document.cookie;
-    var cookie = cookies.split(";");
-    var form = cookie[0].split("=");
-    console.log(form[1]);
+  var iframe = document.getElementById("frame");
+  var cookies = document.cookie;
+  var cookie = cookies.split(";");
+  var form = cookie[0].split("=");
+
+  console.log(form[1]);
+  if (form[1] == null) {
+    iframe.src = "Sub/404.html";
+  } else {
     iframe.src = form[1];
+  }
 }
 
 function loadFrame(subpage) {
-    var iframe = document.getElementById("frame");
-    iframe.src = subpage;
-    document.cookie = `currentFrame=${subpage}`;
+  var iframe = document.getElementById("frame");
+  iframe.src = subpage;
+  document.cookie = `currentFrame=${subpage}; SameSite=None; Secure`;
+  console.log(subpage);
 }
