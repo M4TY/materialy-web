59fb2bc045118504a26fa8db0ff35cacd923e567
M4TY <matyhimmer@gmail.com>
2021-12-17 09:54:12 +0100

    0.1.3

---
+++ Sub/info.md
@@ -0,0 +1,20 @@
+> Disclaimer: by using this site you agree with using cookies
+
+# Changelog
+
+## 0.1.3
+
+- added changelog
+- added version numbers
+- edited flexbox layout
+- fixed 404
+
+## 0.1.2
+
+- revamped web layout
+- added iframe support
+- added cookies for state saving
+
+## 0.1.1
+
+- basic site layout
--- app.js
+++ app.js
@@ -1,20 +1,23 @@
-function loaded() {
-  var iframe = document.getElementById("frame");
-  var cookies = document.cookie;
-  var cookie = cookies.split(";");
-  var form = cookie[0].split("=");
+var version = "0.1.3";
 
-  console.log(form[1]);
-  if (form[1] == null) {
-    iframe.src = "Sub/404.html";
-  } else {
-    iframe.src = form[1];
-  }
+function loaded() {
+    var iframe = document.querySelector("#frame");
+    var cookies = document.cookie;
+    var cookie = cookies.split(";");
+    var form = cookie[0].split("=");
+
+    if (form[1] == null) {
+        iframe.src = "Sub/404.html";
+    } else {
+        iframe.src = form[1];
+    }
+
+    var versionP = document.querySelector(".versionP");
+    versionP.innerHTML = version;
 }
 
 function loadFrame(subpage) {
-  var iframe = document.getElementById("frame");
-  iframe.src = subpage;
-  document.cookie = `currentFrame=${subpage}; SameSite=None; Secure`;
-  console.log(subpage);
+    var iframe = document.querySelector("#frame");
+    iframe.src = subpage;
+    document.cookie = `currentFrame=${subpage}; SameSite=None; Secure`;
 }
