384343889048ab4442ae46dbe1c4ceaf357c02a2
M4TY <matyhimmer@gmail.com>
2022-01-07 12:33:27 +0100

    adada

---
+++ src/components/Content.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { page } from "../store.js";
+</script>
+
+<div class="wrapper">
+  <iframe src={$page} frameborder="0" />
+</div>
+
+<style>
+  .wrapper {
+    width: 100%;
+    height: 94%;
+    background-color: tomato;
+  }
+  .wrapper iframe {
+    width: 100%;
+    height: 100%;
+    background-color: tomato;
+  }
+</style>
---
+++ src/components/Navbar.svelte
@@ -0,0 +1,30 @@
+<nav>
+  <a>Čeština</a>
+  <a>Matematika</a>
+</nav>
+
+<style>
+  @import url("https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;500;700&display=swap");
+  nav {
+    background-color: rgb(35, 32, 37);
+    height: 6%;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+    flex-direction: row;
+  }
+  nav a {
+    font-size: 20px;
+    font-family: "Roboto", sans-serif;
+    text-decoration: none;
+    color: white;
+    transition: 0.25s;
+  }
+  nav a:hover {
+    cursor: pointer;
+    transition: 0.25s;
+    opacity: 0.7;
+  }
+</style>
