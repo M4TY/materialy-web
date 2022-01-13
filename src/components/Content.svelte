0b1087b11f9a72f9946c997ab3543d10da94b735
M4TY <matyhimmer@gmail.com>
2022-01-13 08:09:46 +0100

    ad

--- src/components/Content.svelte
+++ src/components/Content.svelte
@@ -1,42 +1,53 @@
 <script>
-    import { onMount } from "svelte";
-    import { compute_slots } from "svelte/internal";
-    let temp = [];
-    $: notes = [];
+  import { onMount } from "svelte";
+  import { compute_slots } from "svelte/internal";
+  let temp = [];
+  let notes = [];
 
-    function fetchData() {
-        fetch(
-            "https://api.github.com/repos/M4TY/zapisky/git/trees/main?recursive=1"
-        )
-            .then((response) => response.json())
-            .then((data) => {
-                temp = data.tree;
-                filterData(temp);
-            });
-    }
+  function fetchData() {
+    fetch(
+      "https://api.github.com/repos/M4TY/zapisky/git/trees/main?recursive=1"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        temp = data.tree;
+        filterData(temp);
+      });
+  }
 
-    onMount(fetchData);
-    function filterData(data) {
-        data.forEach((element) => {
-            if (element.path.endsWith(".html")) {
-                notes.push(element);
-                console.log(element);
-                console.log(notes.length);
-            }
-        });
-    }
+  onMount(fetchData);
+  function filterData(data) {
+    data.forEach((element) => {
+      if (element.path.endsWith(".html")) {
+        let splitted = element.path.split("/");
+
+        let tempSubj = splitted[0];
+        console.log(tempSubj);
+        let tempName = splitted[1].split(".")[0];
+        let tempLink =
+          "https://raw.githubusercontent.com/M4TY/zapisky/main/" + element.path;
+
+        let obj = {
+          subject: tempSubj,
+          name: tempName,
+          link: tempLink,
+        };
+        notes = [...notes, obj];
+      }
+    });
+  }
 </script>
 
 <div class="grid-wrapper">
-    {#each notes as note}
-        <p>{note}</p>
-    {/each}
+  {#each notes as note}
+    <p>{note.link}</p>
+  {/each}
 </div>
 
 <style>
-    .grid-wrapper {
-        width: 100%;
-        height: 94%;
-        background-color: tomato;
-    }
+  .grid-wrapper {
+    width: 100%;
+    height: 94%;
+    background-color: tomato;
+  }
 </style>
