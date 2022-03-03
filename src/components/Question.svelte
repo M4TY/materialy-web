4a3e4d0523bb0fdf21d6a1e0118a6ffa8e3084ea
Maty <35110087+M4TY@users.noreply.github.com>
2022-03-03 15:20:58 +0100

    finish, ready for design

--- src/components/Question.svelte
+++ src/components/Question.svelte
@@ -2,18 +2,40 @@
 	export let index;
 	export let question;
 	export let answer;
+
+	function removeButton() {}
 </script>
 
 <div class="wrapper">
 	<p>{index}</p>
-	<p>{question}</p>
-	<p>{answer}</p>
+	<input type="text" value={question} />
+	<textarea type="text" value={answer} />
+	<button>🗑️</button>
 </div>
 
 <style>
+	@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@100;400;700&display=swap');
 	.wrapper {
-		height: 100px;
+		padding-top: 10px;
+		padding-bottom: 10px;
+		font-family: 'Montserrat', sans-serif;
 		background-color: white;
-		width: 50%;
+		width: 30%;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;
+		gap: 10px;
+		position: relative;
+	}
+	input {
+		font-family: 'Montserrat', sans-serif;
+	}
+	textarea {
+		font-family: 'Montserrat', sans-serif;
+	}
+	button {
+		position: absolute;
+		right: 5px;
 	}
 </style>
--- src/routes/editQuiz.svelte
+++ src/routes/editQuiz.svelte
@@ -3,6 +3,8 @@
 	import { onMount } from 'svelte';
 	let link;
 	let source = [];
+	let edited = [];
+	let finalText;
 	onMount(() => {
 		let temp = window.location.search;
 		let splitted = temp.split('=');
@@ -13,15 +15,97 @@
 	function fetchData() {
 		fetch(link)
 			.then((res) => res.json())
-			.then((data) => (source = data));
+			.then((data) => {
+				source = data;
+				edited = source;
+			});
+	}
+
+	function removeQuestion(index) {
+		console.log(edited);
+		edited.splice(index, 1);
+		edited = edited;
+		finalText = '';
+	}
+
+	function addQuestion() {
+		let obj = {
+			question: '',
+			answer: ''
+		};
+		edited.push(obj);
+		edited = edited;
+		finalText = '';
+	}
+
+	function exportQuiz() {
+		var json = JSON.stringify(edited);
+		finalText = json;
 	}
 </script>
 
-{#each source as item, i}
-	{#if i === 0}
-		<p>{item.subject}</p>
-		<p>{item.theme}</p>
-	{:else}
-		<Question index={i} question={item.question} answer={item.answer} />
-	{/if}
-{/each}
+<div class="wrapper">
+	{#each edited as item, i}
+		{#if i === 0}
+			<p class="subject">{item.subject}</p>
+			<p class="theme">{item.theme}</p>
+		{:else}
+			<div class="wrapperWindows">
+				<p>{i}</p>
+				<input bind:value={item.question} type="text" />
+				<textarea bind:value={item.answer} type="text" />
+				<button on:click={() => removeQuestion(i)}>🗑️</button>
+			</div>
+		{/if}
+	{/each}
+	<div class="options">
+		<button on:click={addQuestion}>Přidat otázku</button>
+		<button on:click={exportQuiz}>Exportovat</button>
+		<textarea bind:value={finalText} />
+	</div>
+</div>
+
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@100;400;700&display=swap');
+	.subject {
+		font-family: 'Montserrat', sans-serif;
+		margin-left: 20px;
+		margin-top: 20px;
+		font-size: 20px;
+		color: white;
+	}
+	.theme {
+		font-family: 'Montserrat', sans-serif;
+		color: white;
+		margin-left: 20px;
+		font-size: 20px;
+	}
+	.wrapper {
+		display: flex;
+		flex-direction: column;
+		gap: 20px;
+	}
+	.wrapperWindows {
+		padding-top: 10px;
+		padding-bottom: 10px;
+		font-family: 'Montserrat', sans-serif;
+		background-color: white;
+		width: 30%;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;
+		gap: 10px;
+		position: relative;
+	}
+	input {
+		font-family: 'Montserrat', sans-serif;
+	}
+	textarea {
+		font-family: 'Montserrat', sans-serif;
+	}
+	.wrapperWindows button {
+		position: absolute;
+		right: 5px;
+	}
+</style>
