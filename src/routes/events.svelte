2b7fd1209124479c7f37ccfbc51e41d68a39c733
M4TY <matyhimmer@gmail.com>
2022-03-17 09:45:52 +0100

    fix date

--- src/routes/events.svelte
+++ src/routes/events.svelte
@@ -5,9 +5,27 @@
     import {onMount} from "svelte";
     let activeEvents = [];
     function fetchData() {
-        axios.get("https://api-materialy.matyashimmer.eu/events/active").then((res) => res.data).then((data) => activeEvents = data);
+        axios.get("https://api-materialy.matyashimmer.eu/events/active").then((res) => res.data).then((data) => {
+            filterData(data);
+        });
     }
 
+    function filterData(data) {
+        data.forEach(element => {
+            let d = new Date(element.due);
+            d.setDate(d.getDate() - 1)
+            let newObject = {
+                name: element.name,
+                subject: element.subject,
+                due: d.toLocaleDateString()
+            }
+
+            activeEvents.push(newObject);
+
+        })
+
+        activeEvents = activeEvents;
+    }
     onMount(fetchData)
 
 </script>
@@ -15,7 +33,7 @@
 <div class="wrapper">
 <div class="cards">
     {#each activeEvents as event}
-        <ContentCard title="{event.name}" subject="{event.subject}" due="{(new Date(event.due)).toLocaleDateString()}"></ContentCard>
+        <ContentCard title="{event.name}" subject="{event.subject}" due="{event.due}"></ContentCard>
         {/each}
 </div>
 </div>
