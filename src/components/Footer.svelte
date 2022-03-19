ac0e58945501755e24641efb6028263c5cdc5ee3
M4TY <matyhimmer@gmail.com>
2022-03-19 13:58:13 +0100

    version

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,11 +1,17 @@
+<script>
+	import {version} from "../stores/versioning.js";
+</script>
+
 <div class="wrapper">
 	<a href="/halloffame">Síň slávy</a>
 	<a href="https://github.com/M4TY/zapisky" target="_blank" rel="noopener">GitHub repo</a>
+	<p>{version}</p>
 </div>
 
 <style>
 	@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@100;400;700&display=swap');
 	.wrapper {
+		position: relative;
 		font-family: 'Montserrat', sans-serif;
 		color: white;
 		width: 100%;
@@ -27,5 +33,12 @@
 	.wrapper a:hover {
 		opacity: 0.8;
 		transition: all 0.25s;
+	}
+
+	.wrapper p {
+		color: rgb(204, 203, 203);
+		position: absolute;
+		left: 13px;
+		font-size: 14px;
 	}
 </style>
