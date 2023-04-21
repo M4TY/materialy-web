505048044937f7be5b785bbfa46d88f4ca42bde3
Matyáš Himmer <matyhimmer@gmail.com>
2023-04-21 21:21:58 +0200

    end of zapisky

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -12,6 +12,7 @@
     <title>Materiály | Home</title>
 </svelte:head>
 <Navbar notesActive="active" />
+<p>Materiály (kromě kvízů) od března roku 2023 už nejsou aktualizovány. Je používán <a href="https://ripe-periwinkle-82e.notion.site/2022-2023-4138a54b7a154fd2850cbe72dd95ed7a">nový zdroj</a></p>
 <Content />
 {#if $cookies === 'false'}
 	<CookiePopup />
@@ -19,3 +20,21 @@
 <!--{#if $versionSeen !== version}-->
 <!--	<NewVersion />-->
 <!--{/if}-->
+
+<style lang="scss">
+    @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@100;400;700&display=swap');
+
+    p {
+        font-family: 'Montserrat', sans-serif;
+        font-size: 1.5rem;
+        text-align: center;
+        margin-top: 1rem;
+        color: white;
+        font-weight: bold;
+    }
+
+    a {
+      color: #639dca;
+      text-decoration: underline;
+    }
+</style>
